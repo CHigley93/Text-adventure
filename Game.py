bf0a18751f21386114eed3b907286f8efdf978a3
Addison Higley <addison.higley@gmail.com>
2019-05-25 18:21:47 -0500

    Exits as a dictionary, with a helper method

--- Game.py
+++ Game.py
@@ -1,8 +1,12 @@
 import room
 
-
+room1_exits = room.make_exit(n='unlocked')
+room2_exits = room.make_exit(s='unlocked', e='locked')
 #make a dictionary for global rooms that will contain every room as they are instanciated
-room1 = room.Room((0,0), 'n', 'This is the starting room')
-room2 = room.Room((0,1), 's', 'This is a bathroom')
+room1 = room.Room((0,0), room1_exits, 'This is the starting room')
+room2 = room.Room((0,1), room2_exits, 'This is a bathroom')
 #create rooms with locations in tuple format, exits as either n, s, e, or w, and a description of the room
 curr_room = room.global_rooms
+for k, v in room1.exits.iteritems():
+    if v:
+        print("the room has an exit to the {direction}. It is {lock_status}.".format(direction=k,lock_status=v))
--- Room.py
+++ Room.py
@@ -1,5 +1,12 @@
 global_rooms = {}
 
+def make_exit(n=None, s=None, e=None, w=None):
+    exits = {"n": n,
+             "s": s,
+             "e": e,
+             "w": w}
+    return exits
+
 class Room:
     def __init__(self, location, exits, desc):
         self.exits = exits
