d1bb896524983193bb818b17834db2e0d5ddd429
CHigley93 <50812667+CHigley93@users.noreply.github.com>
2019-05-26 15:53:32 -0500

    Delete Room.py

--- Room.py
+++
@@ -1,7 +0,0 @@
-
-class Room:
-    def __init__(self, location, exits, desc):
-        self.exits = exits
-        self.description = desc
-        self.location = location
-        global_rooms[self.location] = self
